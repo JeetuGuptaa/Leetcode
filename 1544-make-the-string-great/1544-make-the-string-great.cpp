7c875675295ab7f390acf7aac3d10b94224df2ee
Jeetu Gupta <92617033+JeetuGuptaa@users.noreply.github.com>
2022-11-08 11:05:26 +0530

    Time: 4 ms (64.51%), Space: 7 MB (17.26%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -2,35 +2,29 @@ class Solution {
 public:
     string makeGood(string s) {
         if(s.length()==1) return s;
-        int i =0, j = 1;
-        while(j<s.length()){
-            int diff = s[i] - s[j];
-            if(abs(diff)==32){
-                s[i] = '0';
-                s[j] = '0';
-                while(i>=0 && s[i]=='0'){
-                    i=i-1;
-                }
-                j+=1;
-                if(i<0){
-                    i = j;
-                    j = i+1;
-                }
-                
+        stack<char> string_so_far;
+        
+        int i =0;
+        while(i<s.size()){
+            if(string_so_far.empty()){
+                string_so_far.push(s[i]);
             }
             else{
-                i+=1;
-                while(i<s.length() && s[i]=='0'){
-                    i+=1;
+                int diff = string_so_far.top() - s[i];
+                if(abs(diff)==32){
+                    string_so_far.pop();
                 }
-                j= i+1;
+                else{
+                    string_so_far.push(s[i]);
+                }
             }
+            i+=1;
         }
-        string ans = "";
-        for(int i=0;i<s.length();i++){
-            if(s[i]!='0'){
-                ans+=s[i];
-            }
+        
+        string ans= "";
+        while(!string_so_far.empty()){
+            ans = string_so_far.top() + ans;
+            string_so_far.pop();
         }
         return ans;
     }
