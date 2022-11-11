b9f9ffbcc9eba05b9571108c6b503f63a0afc398
Jeetu Gupta <92617033+JeetuGuptaa@users.noreply.github.com>
2022-11-11 17:51:31 +0530

    Added solution - LeetHub

--- Primes sum - GFG/primes-sum.cpp
+++ Primes sum - GFG/primes-sum.cpp
@@ -7,50 +7,28 @@ using namespace std;
 // User function Template for C++
 class Solution {
 public:
-    string isSumOfTwo(int N){
-        int i =2;
-        int j;
-        for(j = N;j>=2;j--){
-            int flag = true;
-            for(int k = 2;k*k<=j;k++){
-                if(j%k==0){
-                    flag = false;
-                    break;
-                }
+    
+    string isPrime(int n){
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return "No";
             }
-            if(flag) break;
+        }
+        return "Yes";
+    }
+    
+    
+
+    string isSumOfTwo(int n){
+        if(n<=3) return "No";
+        
+        //if n is even return true
+        else if(n%2==0){
+            return "Yes";
         }
         
-        while(j>=i){
-            if(j+i==N) return "Yes";
-            else if(i+j<N){
-                i++;
-                for( ;i<j;i++){
-                    int flag = true;
-                    for(int k = 2;k*k<=i;k++){
-                        if(i%k==0){
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if(flag) break;
-                    }
-                }
-            else{
-                j--;
-                for(;j>=2;j--){
-            int flag = true;
-            for(int k = 2;k*k<=j;k++){
-                if(j%k==0){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) break;
-        }
-            }
-        }
-        return "No";
+        //if n is odd then one prime must be EVEN since sum of 2 odd numbers is ODD and only even prime is 2
+        return isPrime(n-2);
         
     }
 };
